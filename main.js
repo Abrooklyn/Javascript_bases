51b96dddc5e6cb8904210b2cfb050352810951c6
Unknown <angeliquevannespenne@hotmail.fr>
2018-10-26 16:42:41 +0200

    Premier pas sur JavaScript

---
+++ main.js
@@ -0,0 +1,9 @@
+var prenom = prompt("Entrez votre prénom :");
+alert("Bonjour, " + prenom);
+
+var humeur = prompt("Es-tu prêt?");
+alert("Bien,...Commençons");
+
+var nb = Number(prompt("dis un chiffre : "));
+alert("Tu es sur de choisir " + nb);
+alert("avec le nombre " + nb + ", tu iras trés loin ! Fonce et n'arrète jamais un instant d'y croire !!")
