0cd335fc67dbb2435bc927704ea1926c695387cd
Unknown <angeliquevannespenne@hotmail.fr>
2018-10-29 12:36:57 +0100

    Modifications

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 var prenom = prompt("Entrez votre prénom :");
 alert("Bonjour, " + prenom);
+alert("OK, " + prenom + " On va jouer à un petit jeu");
 
-var humeur = prompt("Es-tu prêt?");
-alert("Bien,...Commençons");
+var nb = Number(prompt("Donne moi un nombre entre 1 et 9:"));
+if (nb = 1,3,5,7,9) {
+    console.log(nb + " est négatif ou nul");
+    alert("dommage ;)")
+}
+else {
+    console.log(nb + " est positif !");
+    alert("avec le nombre " + nb + ", tu iras trés loin ! Fonce et n'arrète jamais un instant d'y croire !!");
+}
 
-var nb = Number(prompt("dis un chiffre : "));
-alert("Tu es sur de choisir " + nb);
-alert("avec le nombre " + nb + ", tu iras trés loin ! Fonce et n'arrète jamais un instant d'y croire !!")
+var x = Number(prompt("Entrez une valeur :"));
+var but = x * 3 - 1;
+but += 5;
+console.log(but);
